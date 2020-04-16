c821a66ece3a8cbeb591fe08e6a6557d55927f86
Iqbal Hossain <iqbal@kotozna.com>
2020-04-16 22:03:21 +0900

    fix

--- pdf_image_test.py
+++ pdf_image_test.py
@@ -1,10 +1,13 @@
 import requests
 import os
-from pprint import pprint
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 url = "https://api-sandbox.fastaccounting.jp/v1.3/convert_to_jpg"
 
-result = requests.post(url, files=dict(file=(basedir + "/testdata.pdf")))
-pprint(result.json())
+data = open(basedir + "/testdata.pdf", 'rb').read()
+headers = {
+    "Content-Type":"application/binary"
+}
+result = requests.post(url, files=dict(file=data), headers=headers)
+print(result.json())
