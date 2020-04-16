8ccb2bcc344e30c16a0c6bf5b7b81e4b218e4c7e
Iqbal Hossain <iqbal@kotozna.com>
2020-04-16 22:20:23 +0900

    fix

--- pdf_image_test.py
+++ pdf_image_test.py
@@ -1,5 +1,14 @@
 import requests
 import os
+import base64
+
+
+def base64_to_image(img_str):
+    img_data = base64.b64decode(img_str)
+    filename = 'test.jpg'
+    with open(filename, 'wb') as f:
+        f.write(img_data)
+    return filename
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -9,4 +18,4 @@ file = {'file': open(basedir + "/testdata.pdf", 'rb')}
 
 result = requests.post(url, files=file)
 result = result.json()
-print(result.get("data", None))
+base64_to_image(result.get("data", None)["image"][0])
