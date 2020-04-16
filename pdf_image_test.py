af065899f7da20ac79ec6ee34f282d6a4e180938
Iqbal Hossain <iqbal@kotozna.com>
2020-04-16 23:04:07 +0900

    fix

--- pdf_image_test.py
+++ pdf_image_test.py
@@ -1,7 +1,7 @@
 import requests
 import os
 import base64
-
+import imghdr
 
 def base64_to_image(img_str):
     img_str += "=" * ((4 - len(img_str) % 4) % 4)
@@ -9,6 +9,7 @@ def base64_to_image(img_str):
     filename = 'test.jpeg'
     with open(filename, 'wb') as f:
         f.write(img_data)
+    print(imghdr.what(basedir + "/" + filename))
     return filename
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -22,7 +23,8 @@ result = result.json()
 img_file = base64_to_image(result.get("data", None)["image"][0])
 
 file = {"file": open(basedir + "/" + img_file, 'rb')}
+header = {'Content-Type': 'application/x-www-form-urlencoded'}
 url = "https://api-sandbox.fastaccounting.jp/v1.3/receipt"
 
-result = requests.post(url, files=file)
+result = requests.post(url, files=file, headers=header)
 print(result.json())
