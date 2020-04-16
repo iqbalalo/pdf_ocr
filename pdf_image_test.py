79ebd0dc1aec26812dd4a84921d417f5000ec63f
Iqbal Hossain <iqbal@kotozna.com>
2020-04-16 23:16:21 +0900

    fix

--- pdf_image_test.py
+++ pdf_image_test.py
@@ -1,16 +1,18 @@
 import requests
 import os
 import base64
+from io import BytesIO
 import imghdr
+from PIL import Image
 
 def base64_to_image(img_str):
     img_str += "=" * ((4 - len(img_str) % 4) % 4)
     img_data = base64.b64decode(img_str)
-    filename = 'test.jpeg'
-    with open(filename, 'wb') as f:
-        f.write(img_data)
-    print(imghdr.what(basedir + "/" + filename))
-    return filename
+    img_data = BytesIO(img_data)
+    img = Image.open(img_data)
+    img.save('test.jpeg', "JPEG")
+    return "test.jpeg"
+
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -22,9 +24,11 @@ result = requests.post(url, files=file)
 result = result.json()
 img_file = base64_to_image(result.get("data", None)["image"][0])
 
+print("image type", imghdr.what(basedir + "/" + img_file))
+
 file = {"file": open(basedir + "/" + img_file, 'rb')}
-header = {'Content-Type': 'application/x-www-form-urlencoded'}
+# header = {'Content-Type': 'application/x-www-form-urlencoded'}
 url = "https://api-sandbox.fastaccounting.jp/v1.3/receipt"
 
-result = requests.post(url, files=file, headers=header)
+result = requests.post(url, files=file)
 print(result.json())
