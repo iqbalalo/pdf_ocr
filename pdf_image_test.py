3c12db313db5df42ef1b10750ac3848fa5e46ecd
Iqbal Hossain <iqbal@kotozna.com>
2020-04-16 22:37:22 +0900

    fix

--- pdf_image_test.py
+++ pdf_image_test.py
@@ -1,14 +1,24 @@
 import requests
 import os
 import base64
+import re
+
+
+def decode_base64(data, altchars=b'+/'):
+    data = re.sub(rb'[^a-zA-Z0-9%s]+' % altchars, b'', data)  # normalize
+    missing_padding = len(data) % 4
+    if missing_padding:
+        data += b'='* (4 - missing_padding)
+    return base64.b64decode(data, altchars)
 
 
 def base64_to_image(img_str):
-    img_data = base64.urlsafe_b64decode(img_str)
+    img_data = decode_base64(img_str)
     filename = 'test.jpg'
     with open(filename, 'wb') as f:
         f.write(img_data)
     return filename
+
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -18,4 +28,5 @@ file = {'file': open(basedir + "/testdata.pdf", 'rb')}
 
 result = requests.post(url, files=file)
 result = result.json()
+print(result.get("data", None)["image"][0])
 base64_to_image(result.get("data", None)["image"][0])
