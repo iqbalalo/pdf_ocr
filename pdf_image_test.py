e962a69248b58050ed936a3e71a94b39ca08ade4
Iqbal Hossain <iqbal@kotozna.com>
2020-04-16 21:55:42 +0900

    fix

--- pdf_image_test.py
+++ pdf_image_test.py
@@ -5,5 +5,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 url = "https://api-sandbox.fastaccounting.jp/v1.3/convert_to_jpg"
 
-result = requests.post(url, files={'file': basedir + "/testdata.pdf"})
+result = requests.post(url, files=dict(file=(basedir + "/testdata.pdf")))
 print(result.json())
