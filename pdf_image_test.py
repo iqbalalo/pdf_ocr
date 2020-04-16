87d7be08c88a6af029d325fdd04237dd7039b40a
Iqbal Hossain <iqbal@kotozna.com>
2020-04-16 21:34:33 +0900

    fix pdf_image_test

--- pdf_image_test.py
+++ pdf_image_test.py
@@ -1,16 +1,9 @@
+import requests
 import os
-import pycurl
-from io import BytesIO
-
-buffer = BytesIO()
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+
 url = "https://api-sandbox.fastaccounting.jp/v1.3/convert_to_jpg"
 
-c = pycurl.Curl()
-c.setopt(c.URL, 'https://httpbin.org/post')
-c.setopt(c.HTTPPOST, [("file", (c.FORM_FILE, basedir + "/testdata.pdf"))])
-c.perform()
-c.close()
-body = buffer.getvalue()
-print(body)
+result = requests.post(url, files={'file': basedir + "/testdata.pdf"})
+print(result.json())
